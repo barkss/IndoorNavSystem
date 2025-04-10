db9a3d967a849c25c1410bc548b30cb73bfae746
John Baclayon <johnmichael.baclayon@cit.edu>
2025-04-11 04:40:43 +0800

    Add new frontend components and assets to public folder

---
+++ src/components/CardItem.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CardItem(props) {
+  return (
+      <>
+        <li className='cards__item'>
+          <Link className='cards__item__link' to={props.path}>
+            {props.mapEmbed ? (
+                <>
+                  <div className="map-container">
+                    <iframe
+                        src={props.mapEmbed}
+                        width="100%"
+                        height="300"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                  <div className="map-text">
+                    <h5 className='cards__item__text'>{props.text}</h5>
+                  </div>
+                </>
+            ) : (
+                <>
+
+                <figure className='cards__item__pic-wrap' data-category={props.label}>
+              <img
+                  className='cards__item__img'
+                  alt='Travel Image'
+                  src={props.src}
+              />
+            </figure>
+            <div className='cards__item__info'>
+              <h5 className='cards__item__text'>{props.text}</h5>
+            </div>
+                </>
+            )}
+          </Link>
+        </li>
+      </>
+  );
+}
+
+export default CardItem;
