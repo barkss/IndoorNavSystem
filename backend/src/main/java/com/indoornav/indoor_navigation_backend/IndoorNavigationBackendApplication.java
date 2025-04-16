2ac75ad3fff1a4400c776b464fc2a1e9bf0724ba
John Baclayon <johnmichael.baclayon@cit.edu>
2025-04-16 17:56:00 +0800

    Add initial Spring Boot backend project

---
+++ backend/src/main/java/com/indoornav/indoor_navigation_backend/IndoorNavigationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.indoornav.indoor_navigation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorNavigationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorNavigationBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/indoornav/indoor_navigation_backend/IndoorNavigationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indoornav.indoor_navigation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorNavigationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
